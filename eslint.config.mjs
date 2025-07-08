cc3f21422d559471f86d4e25b668089cf51079d4
zodiacfireworks <martin.vuelta@gmail.com>
2025-07-08 09:52:09 -0500

    Update: enhance eslint config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -4,17 +4,24 @@ import prettier from 'eslint-config-prettier';
 
 import { includeIgnoreFile } from '@eslint/compat';
 import ts from 'typescript-eslint';
+import js from '@eslint/js';
 import typescriptEslint from "@typescript-eslint/eslint-plugin";
 import tsParser from "@typescript-eslint/parser";
+import globals from 'globals';
 
 const gitignorePath = fileURLToPath(new URL('./.gitignore', import.meta.url));
 
 export default ts.config(
     includeIgnoreFile(gitignorePath),
+    js.configs.recommended,
     ...ts.configs.recommended,
     prettier,
     {
         files: ["**/*.ts"],
+        rules: {
+            // Preffer over escaped characters
+            "no-useless-escape": "off",
+        }
     },
     {
         plugins: {
@@ -24,6 +31,7 @@ export default ts.config(
             parser: tsParser,
             ecmaVersion: 2022,
             sourceType: "module",
+            globals: { ...globals.node }
         },
         rules: {
             "@typescript-eslint/naming-convention": ["warn", {
