689f600d2adc26e12e07a8af0480c09511bfa77b
zodiacfireworks <martin.vuelta@gmail.com>
2025-10-26 22:36:22 -0500

    Update: refactor extension to use new classes

--- src/extension.ts
+++ src/extension.ts
@@ -1,8 +1,12 @@
 import * as vscode from 'vscode';
 import { extensionLogger, extensionOutputChannel, extensionTraceOutputChannel } from './logger';
-import { startLanguageServer, stopLanguageServer } from './language-server';
+import { LarkClient } from './language-server';
 import { getLanguageServerInfo } from './settings';
-import { removeBundledEnvironment } from './python';
+import { PythonExtension, PythonInterpreter, PythonEnvironment } from './python';
+import { LarkTerminalProvider } from './providers/LarkTerminalProvider';
+import { LarkRuleProvider } from './providers/LarkRuleProvider';
+
+let larkClient: LarkClient;
 
 export async function activate(context: vscode.ExtensionContext): Promise<void> {
     context.subscriptions.push(extensionOutputChannel);
@@ -10,7 +14,35 @@ export async function activate(context: vscode.ExtensionContext): Promise<void> 
 
     const { name: languageServerName, module: languageServerModule } = getLanguageServerInfo();
 
-    extensionLogger.log('Activating Lark Language Server extension...');
+    extensionLogger.log(`Activating ${languageServerName} extension...`);
+
+    const pythonExtension = new PythonExtension();
+    const pythonInterpreter = new PythonInterpreter(pythonExtension);
+    const pythonEnvironment = new PythonEnvironment(context, pythonInterpreter);
+
+    const larkTerminalProvider = new LarkTerminalProvider(
+        vscode.window.activeTextEditor?.document!
+    );
+
+    const larkRuleProvider = new LarkRuleProvider(vscode.window.activeTextEditor?.document!);
+
+    larkClient = new LarkClient(
+        context,
+        pythonEnvironment,
+        {
+            provideDocumentSymbols: async (document, token, next) => {
+                const response = await next(document, token);
+
+                larkTerminalProvider.captureTerminals(document, response ?? []);
+                larkRuleProvider.captureRules(document, response ?? []);
+
+                return response;
+            }
+        },
+        extensionOutputChannel,
+        extensionTraceOutputChannel
+    );
+    await larkClient.initialize();
 
     const showLogsCommand = vscode.commands.registerCommand(
         `${languageServerModule}.showLogs`,
@@ -22,9 +54,10 @@ export async function activate(context: vscode.ExtensionContext): Promise<void> 
     const restartCommand = vscode.commands.registerCommand(
         `${languageServerModule}.restartServer`,
         async () => {
-            extensionLogger.log('Restarting Lark Language Server...');
-            await stopLanguageServer();
-            await startLanguageServer(context);
+            extensionLogger.log(`Restarting the ${languageServerName} Language Server...`);
+            await larkClient.stop();
+            await larkClient.initialize();
+            await larkClient.start();
             vscode.window.showInformationMessage(`${languageServerName} Language Server restarted`);
         }
     );
@@ -32,8 +65,8 @@ export async function activate(context: vscode.ExtensionContext): Promise<void> 
     const removeCommand = vscode.commands.registerCommand(
         `${languageServerModule}.removeBundledEnvironment`,
         async () => {
-            extensionLogger.log('Removing bundled environment...');
-            await removeBundledEnvironment(context);
+            extensionLogger.log(`Removing bundled environment for ${languageServerName}...`);
+            await pythonEnvironment.remove();
             vscode.window.showInformationMessage(`Bundled environment removed`);
         }
     );
@@ -41,10 +74,16 @@ export async function activate(context: vscode.ExtensionContext): Promise<void> 
     context.subscriptions.push(showLogsCommand);
     context.subscriptions.push(restartCommand);
     context.subscriptions.push(removeCommand);
+    context.subscriptions.push(
+        vscode.window.registerTreeDataProvider('lark.terminals', larkTerminalProvider)
+    );
+    context.subscriptions.push(
+        vscode.window.registerTreeDataProvider('lark.rules', larkRuleProvider)
+    );
 
-    await startLanguageServer(context);
+    await larkClient.start();
 }
 
 export function deactivate(): Thenable<void> | undefined {
-    return stopLanguageServer();
+    return larkClient.stop();
 }
